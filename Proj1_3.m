dcca8c2d40a3994f9ad90ae746fb84757b493708
jguan4 <jiajingguan@gmail.com>
2018-09-20 11:43:13 -0400

    Basic codes
    These codes don't exactly work yet, they still need tweaks

---
+++ Proj1_3.m
@@ -0,0 +1,107 @@
+% Test example for nonlinear least squares fitting
+% Expected solution: a = 2;
+
+close all;
+clear all;
+plotting=1;
+
+global xexp
+global texp
+
+%Experimental data
+xexp = [1.1,-0.98,-0.3]';
+texp = [0:2];
+
+method =2; % 1=fminsearch, 2=Newton
+
+if (method==1)
+    a_opt = 1;
+    % Finding the parameter values minimizing SSE error using built-in function
+    [a_opt,fval,exitflag,output] = fminsearch(@SSE,0)
+    
+else
+    %Newton-Raphson method
+    aa(1,:)=[0 0];
+    delta1 = 1e-10;
+    delta2 = 1e-10;
+    Nsteps = 50;
+    
+    for k=1:Nsteps
+        
+        %Need to solve ODE on a finer grid
+        tt = linspace(texp(1),texp(end),100);
+        zt0 =[0.9;0.1;0;0;0;0]; %initial data for the sensitivity system
+        [tspan,zz] = ode45(@(t,z)sens(t,z,aa),tt,zt0);
+        z = interp1(tt,zz,texp); %getting back to the coarser grid to compare with xexp
+        
+        %Jacobian calculation
+        J1 = [z(:,3:4)];
+        J2 = [z(:,5:6)];
+        
+        %Hessian with weights
+        H = J1'*J1 + J2'*J2;
+        g = J1'*(xexp(1,:)' - z(:,1)) + J2'*(xexp(2,:)' - z(:,2));
+        
+        
+        %Newton direction = inv(H)*g, but
+        %Better to use slash command for a more stable calculation
+        aa(k+1,:) = aa(k,:) + (H\g)';
+        a_opt = aa(k+1,:);
+        
+        if (norm(aa(k+1,:)-aa(k,:))<delta1) | (abs(SSE(aa(k+1,:))-SSE(aa(k,:)))<delta2)
+            break;
+        end
+    end
+    
+    figure(10);
+    plot(1:length(aa), aa,'ro');
+    
+end
+
+
+if (plotting==1)
+    
+    xt0 =a_opt(2);
+    [tspan,x] = ode45(@(t,x)system(t,x,a_opt),texp,xt0);
+    
+    figure(1);
+    set(gca,'FontName','Arial','FontSize',14,'FontWeight','Bold','LineWidth', 1);
+    hold on;
+    axis square;
+    title('y(1) vs. t');
+    plot(tspan,x,'r.');
+    plot(texp,xexp,'bo');
+    legend('ODE','data');
+    
+end
+
+function z = SSE(a)
+global texp
+global xexp
+
+xt0 =[0.9 0.1];
+[tspan,x] = ode45(@(t,x)system(t,x,a),texp,xt0);
+
+z = norm(x-xexp,2);
+
+end
+
+function dxdt=system(t,x,a)
+
+dxdt(1) = -a(1)*x(1)*x(2);
+dxdt(2) = a(1)*x(1)*x(2) - a(2)*x(2);
+
+dxdt = dxdt';
+
+end
+
+
+function dzdt = sens(t,z,a)
+dzdt(1) = -a(1)*z(1)*z(2);
+dzdt(2) = a(1)*z(1)*z(2)-a(2)*z(2);
+dzdt(3) = -z(1)*z(2) - a(1)*z(2)*z(3) - a(1)*z(2)*z(4);
+dzdt(4) = z(1)*z(2) + a(1)*z(2)*z(3) + (a(1)*z(1)-a(2))*z(4);
+dzdt(5) = -a(1)*z(2)*z(5) -a(1)*z(1)*z(6);
+dzdt(6) = -z(2) + a(1)*z(2)*z(5) + (a(1)*z(1)-a(2))*z(6);
+dzdt = dzdt';
+end
